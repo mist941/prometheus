dc70ed40c5756727e190bd30fc0784c8d5276196
mist941 <statkevich.ivan999@gmail.com>
2025-07-02 09:40:07 +0300

    Branch: main
    Files changed:
    python/prom-test.py
    python/requirements.txt
    
    Description:
    Add Prometheus client test script with metrics demonstration

--- python/prom-test.py
+++ python/prom-test.py
@@ -3,13 +3,14 @@ import time
 from prometheus_client import Summary, start_http_server, Counter, Gauge
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-MY_COUNTER = Counter('my_counter', 'My counter')
+MY_COUNTER = Counter('my_counter', ["name", "age"])
 MY_GAUGE = Gauge('my_gauge', 'My gauge')
 
 @REQUEST_TIME.time()
 def process_request(t):
     """A dummy function that takes some time."""
-    MY_COUNTER.inc(5)
+    MY_COUNTER.labels("John", 20).inc(5)
+    MY_COUNTER.labels(name="Jane", age=21).inc(10)
     MY_GAUGE.set(10)
     MY_GAUGE.inc(5)
     MY_GAUGE.dec(3)
