52e533a9adb972ba0b09aad6566202663b2c197a
mist941 <statkevich.ivan999@gmail.com>
2025-07-02 09:32:55 +0300

    Branch: main
    Files changed:
    python/prom-test.py
    
    Description:
    Add Prometheus monitoring test script with Counter, Gauge, and Summary metrics

--- python/prom-test.py
+++ python/prom-test.py
@@ -1,5 +1,25 @@
+import random
+import time
+from prometheus_client import Summary, start_http_server, Counter, Gauge
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+MY_COUNTER = Counter('my_counter', 'My counter')
+MY_GAUGE = Gauge('my_gauge', 'My gauge')
+
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    MY_COUNTER.inc(5)
+    MY_GAUGE.set(10)
+    MY_GAUGE.inc(5)
+    MY_GAUGE.dec(3)
+    time.sleep(t)
+    
 def main():
-  print("Hello")
+    start_http_server(8000)
+    process_request(random.random())
+    while True:
+        A=1
 
 if __name__ == "__main__":
   main()
